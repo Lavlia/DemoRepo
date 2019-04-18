1ad8cdea37b416ce9c13b27fdf27afb5696a647f
Lavlia <lavinia.bogdanescu@gmail.com>
2019-04-18 14:05:16 +0300

    added another method T1 in login page

--- src/main/java/pageObject/LoginPage.java
+++ src/main/java/pageObject/LoginPage.java
@@ -8,4 +8,8 @@ public class LoginPage {
     public void testLoginPage1() {
         System.out.println("Login page1");
     }
-}
+
+    public void testLoginPageT1() {
+        System.out.println("Login pageT1");
+
+    }
