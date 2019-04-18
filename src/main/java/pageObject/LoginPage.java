76289d28ee51b2f62120b182431d08108c65f1e1
Lavlia <lavinia.bogdanescu@gmail.com>
2019-04-18 13:08:07 +0300

    added search page feature code

--- src/main/java/pageObject/LoginPage.java
+++ src/main/java/pageObject/LoginPage.java
@@ -4,4 +4,8 @@ public class LoginPage {
     public void testLoginPage() {
         System.out.println("Login page");
     }
+
+    public void testLoginPage1() {
+        System.out.println("Login page1");
+    }
 }
---
+++ src/main/java/pageObject/SearchPage.java
@@ -0,0 +1,7 @@
+package pageObject;
+
+public class SearchPage {
+    public void testSearchPage() {
+        System.out.println("Search page");
+    }
+}
